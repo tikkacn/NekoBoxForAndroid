cf06a5494d8d14ee01d83fc28ab03ab96a7d55ad
tikkacn <tikkacn@qq.com>
2025-12-16 10:13:00 +0800

    Update MainActivity.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
@@ -339,16 +339,6 @@ class MainActivity : ThemedActivity(),
             R.id.nav_traffic -> displayFragment(WebviewFragment())
             R.id.nav_tools -> displayFragment(ToolsFragment())
             R.id.nav_logcat -> displayFragment(LogcatFragment())
-            R.id.nav_faq -> {
-                launchCustomTab("https://matsuridayo.github.io/")
-                return false
-            }
-
-            R.id.nav_about -> displayFragment(AboutFragment())
-            R.id.nav_tuiguang -> {
-                launchCustomTab("https://neko-box.pages.dev/喵")
-                return false
-            }
 
             else -> return false
         }
